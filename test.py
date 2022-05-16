f0a098c04c92e661ec44f058b4e82e9a7b105e32
serkancam <sekocam@gmail.com>
2022-05-16 19:42:44 +0300

    g

--- test.py
+++ test.py
@@ -1,34 +1,42 @@
 import cv2
-import numpy as np
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
+
+# For webcam input:
 cap = cv2.VideoCapture(0)
-ret,frame1 = cap.read()
-ret,frame2 = cap.read()
-while cap.isOpened():
-    fark = cv2.absdiff(frame1,frame2)
-    gri = cv2.cvtColor(fark,cv2.COLOR_BGR2GRAY)
-    yumusak = cv2.GaussianBlur(gri,(5,5),0)
-    t,sbResim = cv2.threshold(yumusak,20,255,cv2.THRESH_BINARY)
-    yayilmis = cv2.dilate(sbResim,None,iterations=3)
-    konturlar,_ = cv2.findContours(yayilmis,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    for kontur in konturlar:
-        (x,y,w,h) = cv2.boundingRect(kontur)
-        # print(cv2.contourArea(kontur))
-        if cv2.contourArea(kontur)<5000:
-            continue
-        cv2.rectangle(frame1,(x,y),(x+w,y+h),(0,0,255),2)
-        cv2.putText(frame1,"Hareket algilandi",(10,30),cv2.FONT_HERSHEY_SIMPLEX,1,(155,155,0),3)
-    # print(konturlar)
-    # ret,frame1 = cap.read()
-    cv2.imshow("Kamera",frame1)
-    # cv2.imshow("fark",fark)
-    # cv2.imshow("gri farkl",gri)
-    # cv2.imshow("gri farkli blur",gri)
-    # cv2.imshow("gri farkli blur siyah beyaz",sbResim)
-    cv2.imshow("gri farkli blur siyah beyaz dilate",yayilmis)
-    frame1 = frame2
-    ret,frame2 = cap.read()
-    if cv2.waitKey(40) == 27:
-        break
-cv2.destroyAllWindows()
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+    # Flip the image horizontally for a selfie-view display.
+    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
 cap.release()
